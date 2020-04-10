340799dbfb0f4c39439b3fe592857bbb0c108a83
glebkiselev <glebkiselev777@mail.ru>
2020-04-10 11:54:48 +0300

    new tasks

--- build/lib/mapcore/planning/parsers/parser_common.py
+++
@@ -1,7 +0,0 @@
-#
-# This file is part of pyperplan.
-#
-# define the set of special characters
-whiteSpace = set([' ', '\n', '\t'])
-comment = set([';'])
-reserved = set([':', ')', '(']).union(comment)
